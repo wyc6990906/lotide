5430e200b075b84e18daa1b5ba4a3e46231cb189
Dylan Wang <wyc6990906@hotmail.com>
2022-05-31 16:07:24 -0600

    fix eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -7,21 +7,20 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (arrayOne, arrayTwo) {
-  let equal = false;
-  if (arrayOne.length !== arrayTwo.length) {
-    equal = false
-  } else {
-    for (let i = 0; i < arrayOne.length; i++) {
-      equal = arrayOne[i] === arrayTwo[i];
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
     }
   }
-  return equal;
-}
+  return true;
+};
 
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
