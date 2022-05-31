d76e6d43a7546855c9f625a5852a0876f071ff27
Yichuan  Wang <wyc6990906@hotmail.com>
2022-05-31 21:01:41 +0000

    eqArray

--- head.js
+++ head.js
@@ -17,9 +17,6 @@ const head = function (array) {
 
 
 // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), undefined);
