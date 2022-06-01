16e165ec286c5b183fad363b40d2c9872f4f892c
Dylan Wang <wyc6990906@hotmail.com>
2022-06-01 16:15:21 -0600

    middle.js w01d03

--- middle.js
+++ middle.js
@@ -43,7 +43,7 @@ const middle = function (array) {
 // TEST CODE
 // assertArraysEqual(middle([1]), [])
 // assertArraysEqual(middle([1, 2]), [])
-// assertArraysEqual(middle([1, 2, 3]), [2])
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+assertArraysEqual(middle([1, 2, 3]), [2])
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
