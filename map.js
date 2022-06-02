d9375b9c04f37f43db62db94a9e31c41db14f5cf
Dylan Wang <wyc6990906@hotmail.com>
2022-06-02 14:28:21 -0600

    Implement Map W01D04

---
+++ map.js
@@ -0,0 +1,30 @@
+const words = ["ground", "control", "to", "major", "tom"];
+const map = function (array, callback) {
+  const results = []
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅️Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑️Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+};
+
+assertArraysEqual(map(words, word => word[0]),['g','c','t','m','t'])
