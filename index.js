34465694dd53872c2b6bd6a5d9f6c65d655421de
Dylan Wang <wyc6990906@hotmail.com>
2022-06-06 15:41:39 -0600

    refactored assertObjectsEqual.js and add test file

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const eqArray = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
+const assertEqual = require('./assertEqual')
+const assertObjectsEqual = require('./assertObjectsEqual')
+const eqObjects = require('./eqObjects')
+
+
+module.exports = {
+  head, tail, middle, eqArray, assertArraysEqual, assertEqual, eqObjects
+}
