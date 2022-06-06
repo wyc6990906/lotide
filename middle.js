09a695a3c0cfb8404b716d6348b4c5381dbf685f
Dylan Wang <wyc6990906@hotmail.com>
2022-06-06 10:54:06 -0600

    refactored middle.js and add test file

--- middle.js
+++ middle.js
@@ -36,14 +36,15 @@ const middle = function (array) {
     resultArr.push(array[index - 1])
     resultArr.push(array[index])
   }
-  console.log(resultArr)
   return resultArr
 }
 
 // TEST CODE
 // assertArraysEqual(middle([1]), [])
 // assertArraysEqual(middle([1, 2]), [])
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+// assertArraysEqual(middle([1, 2, 3]), [2])
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3])
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3])
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+
+module.exports = middle
